44557414119883a89b02ef1a237fe9a23acb5d8d
hoodielive <hoodielive@gmail.com>
2019-07-30 07:54:18 -0400

    solve for factorial with recursion

--- python/recursion-func.py
+++ python/recursion-func.py
@@ -6,3 +6,22 @@ def factorial(n):
     return result
 
 print(factorial(5))
+
+# turn it to a recursive function 
+
+def factorial(n):
+    if n <= 1:
+        return n
+    return n * factorial(n-1)
+print(factorial(4))
+
+
+def fib(n):
+    if n == 0: 
+        return 0
+    elif n == 1: 
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+print(fib(7))
